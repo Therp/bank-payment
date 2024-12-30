36589ac7dfd74ab3610d139d2b11e7fb40c4a1b3
Danny de Jong <ddejong@therp.nl>
2024-12-30 11:18:13 +0100

    [FIX] account_payment_record: Disable post-migration script due to issues

--- account_payment_order/migrations/14.0.2.0.0/post-migration.py
+++ account_payment_order/migrations/14.0.2.0.0/post-migration.py
@@ -162,17 +162,18 @@ def _insert_payment_line_payment_link(env):
 
 @openupgrade.migrate()
 def migrate(env, version):
-    openupgrade.logged_query(
-        env.cr, "ALTER TABLE account_payment ALTER move_id DROP NOT NULL"
-    )
-    _insert_account_payments(env)
-    _create_hooks(env)
-    create_moves_from_orphan_account_payments(env)
-    openupgrade.logged_query(
-        env.cr, "ALTER TABLE account_payment ALTER move_id SET NOT NULL"
-    )
-    _delete_hooks(env)
-    _insert_payment_line_payment_link(env)
-    openupgrade.delete_records_safely_by_xml_id(
-        env, ["account_payment_order.bank_payment_line_company_rule"]
-    )
+    # openupgrade.logged_query(
+    #     env.cr, "ALTER TABLE account_payment ALTER move_id DROP NOT NULL"
+    # )
+    # _insert_account_payments(env)
+    # _create_hooks(env)
+    # create_moves_from_orphan_account_payments(env)
+    # openupgrade.logged_query(
+    #     env.cr, "ALTER TABLE account_payment ALTER move_id SET NOT NULL"
+    # )
+    # _delete_hooks(env)
+    # _insert_payment_line_payment_link(env)
+    # openupgrade.delete_records_safely_by_xml_id(
+    #     env, ["account_payment_order.bank_payment_line_company_rule"]
+    # )
+    pass
